cd8617d772c09956010870df615cf0d02c2d804e
Jason Ruesch <jruesch@degreed.com>
2023-12-24 13:45:41 -0500

    chore(repo): add graphql codegen

---
+++ libs/bills/data-access/src/gql/graphql.ts
@@ -0,0 +1,451 @@
+/* eslint-disable */
+export type Maybe<T> = T | null;
+export type InputMaybe<T> = Maybe<T>;
+export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
+export type MakeEmpty<T extends { [key: string]: unknown }, K extends keyof T> = { [_ in K]?: never };
+export type Incremental<T> = T | { [P in keyof T]?: P extends ' $fragmentName' | '__typename' ? T[P] : never };
+/** All built-in and custom scalars, mapped to their actual values */
+export type Scalars = {
+  ID: { input: string; output: string; }
+  String: { input: string; output: string; }
+  Boolean: { input: boolean; output: boolean; }
+  Int: { input: number; output: number; }
+  Float: { input: number; output: number; }
+  /** A high precision floating point value represented as a string */
+  BigFloat: { input: any; output: any; }
+  /** An arbitrary size integer represented as a string */
+  BigInt: { input: any; output: any; }
+  /** An opaque string using for tracking a position in results during pagination */
+  Cursor: { input: any; output: any; }
+  /** A date wihout time information */
+  Date: { input: any; output: any; }
+  /** A date and time */
+  Datetime: { input: any; output: any; }
+  /** A Javascript Object Notation value serialized as a string */
+  JSON: { input: any; output: any; }
+  /** Any type not handled by the type system */
+  Opaque: { input: any; output: any; }
+  /** A time without date information */
+  Time: { input: any; output: any; }
+  /** A universally unique identifier */
+  UUID: { input: any; output: any; }
+};
+
+/** Boolean expression comparing fields on type "BigFloat" */
+export type BigFloatFilter = {
+  eq?: InputMaybe<Scalars['BigFloat']['input']>;
+  gt?: InputMaybe<Scalars['BigFloat']['input']>;
+  gte?: InputMaybe<Scalars['BigFloat']['input']>;
+  in?: InputMaybe<Array<Scalars['BigFloat']['input']>>;
+  is?: InputMaybe<FilterIs>;
+  lt?: InputMaybe<Scalars['BigFloat']['input']>;
+  lte?: InputMaybe<Scalars['BigFloat']['input']>;
+  neq?: InputMaybe<Scalars['BigFloat']['input']>;
+};
+
+/** Boolean expression comparing fields on type "BigInt" */
+export type BigIntFilter = {
+  eq?: InputMaybe<Scalars['BigInt']['input']>;
+  gt?: InputMaybe<Scalars['BigInt']['input']>;
+  gte?: InputMaybe<Scalars['BigInt']['input']>;
+  in?: InputMaybe<Array<Scalars['BigInt']['input']>>;
+  is?: InputMaybe<FilterIs>;
+  lt?: InputMaybe<Scalars['BigInt']['input']>;
+  lte?: InputMaybe<Scalars['BigInt']['input']>;
+  neq?: InputMaybe<Scalars['BigInt']['input']>;
+};
+
+/** Boolean expression comparing fields on type "Boolean" */
+export type BooleanFilter = {
+  eq?: InputMaybe<Scalars['Boolean']['input']>;
+  is?: InputMaybe<FilterIs>;
+};
+
+/** Boolean expression comparing fields on type "Date" */
+export type DateFilter = {
+  eq?: InputMaybe<Scalars['Date']['input']>;
+  gt?: InputMaybe<Scalars['Date']['input']>;
+  gte?: InputMaybe<Scalars['Date']['input']>;
+  in?: InputMaybe<Array<Scalars['Date']['input']>>;
+  is?: InputMaybe<FilterIs>;
+  lt?: InputMaybe<Scalars['Date']['input']>;
+  lte?: InputMaybe<Scalars['Date']['input']>;
+  neq?: InputMaybe<Scalars['Date']['input']>;
+};
+
+/** Boolean expression comparing fields on type "Datetime" */
+export type DatetimeFilter = {
+  eq?: InputMaybe<Scalars['Datetime']['input']>;
+  gt?: InputMaybe<Scalars['Datetime']['input']>;
+  gte?: InputMaybe<Scalars['Datetime']['input']>;
+  in?: InputMaybe<Array<Scalars['Datetime']['input']>>;
+  is?: InputMaybe<FilterIs>;
+  lt?: InputMaybe<Scalars['Datetime']['input']>;
+  lte?: InputMaybe<Scalars['Datetime']['input']>;
+  neq?: InputMaybe<Scalars['Datetime']['input']>;
+};
+
+export enum FilterIs {
+  NotNull = 'NOT_NULL',
+  Null = 'NULL'
+}
+
+/** Boolean expression comparing fields on type "Float" */
+export type FloatFilter = {
+  eq?: InputMaybe<Scalars['Float']['input']>;
+  gt?: InputMaybe<Scalars['Float']['input']>;
+  gte?: InputMaybe<Scalars['Float']['input']>;
+  in?: InputMaybe<Array<Scalars['Float']['input']>>;
+  is?: InputMaybe<FilterIs>;
+  lt?: InputMaybe<Scalars['Float']['input']>;
+  lte?: InputMaybe<Scalars['Float']['input']>;
+  neq?: InputMaybe<Scalars['Float']['input']>;
+};
+
+/** Boolean expression comparing fields on type "ID" */
+export type IdFilter = {
+  eq?: InputMaybe<Scalars['ID']['input']>;
+};
+
+/** Boolean expression comparing fields on type "Int" */
+export type IntFilter = {
+  eq?: InputMaybe<Scalars['Int']['input']>;
+  gt?: InputMaybe<Scalars['Int']['input']>;
+  gte?: InputMaybe<Scalars['Int']['input']>;
+  in?: InputMaybe<Array<Scalars['Int']['input']>>;
+  is?: InputMaybe<FilterIs>;
+  lt?: InputMaybe<Scalars['Int']['input']>;
+  lte?: InputMaybe<Scalars['Int']['input']>;
+  neq?: InputMaybe<Scalars['Int']['input']>;
+};
+
+/** The root type for creating and mutating data */
+export type Mutation = {
+  __typename?: 'Mutation';
+  /** Deletes zero or more records from the `bills` collection */
+  deleteFrombillsCollection: BillsDeleteResponse;
+  /** Deletes zero or more records from the `profiles` collection */
+  deleteFromprofilesCollection: ProfilesDeleteResponse;
+  /** Adds one or more `bills` records to the collection */
+  insertIntobillsCollection?: Maybe<BillsInsertResponse>;
+  /** Adds one or more `profiles` records to the collection */
+  insertIntoprofilesCollection?: Maybe<ProfilesInsertResponse>;
+  /** Updates zero or more records in the `bills` collection */
+  updatebillsCollection: BillsUpdateResponse;
+  /** Updates zero or more records in the `profiles` collection */
+  updateprofilesCollection: ProfilesUpdateResponse;
+};
+
+
+/** The root type for creating and mutating data */
+export type MutationDeleteFrombillsCollectionArgs = {
+  atMost?: Scalars['Int']['input'];
+  filter?: InputMaybe<BillsFilter>;
+};
+
+
+/** The root type for creating and mutating data */
+export type MutationDeleteFromprofilesCollectionArgs = {
+  atMost?: Scalars['Int']['input'];
+  filter?: InputMaybe<ProfilesFilter>;
+};
+
+
+/** The root type for creating and mutating data */
+export type MutationInsertIntobillsCollectionArgs = {
+  objects: Array<BillsInsertInput>;
+};
+
+
+/** The root type for creating and mutating data */
+export type MutationInsertIntoprofilesCollectionArgs = {
+  objects: Array<ProfilesInsertInput>;
+};
+
+
+/** The root type for creating and mutating data */
+export type MutationUpdatebillsCollectionArgs = {
+  atMost?: Scalars['Int']['input'];
+  filter?: InputMaybe<BillsFilter>;
+  set: BillsUpdateInput;
+};
+
+
+/** The root type for creating and mutating data */
+export type MutationUpdateprofilesCollectionArgs = {
+  atMost?: Scalars['Int']['input'];
+  filter?: InputMaybe<ProfilesFilter>;
+  set: ProfilesUpdateInput;
+};
+
+export type Node = {
+  /** Retrieves a record by `ID` */
+  nodeId: Scalars['ID']['output'];
+};
+
+/** Boolean expression comparing fields on type "Opaque" */
+export type OpaqueFilter = {
+  eq?: InputMaybe<Scalars['Opaque']['input']>;
+  is?: InputMaybe<FilterIs>;
+};
+
+/** Defines a per-field sorting order */
+export enum OrderByDirection {
+  /** Ascending order, nulls first */
+  AscNullsFirst = 'AscNullsFirst',
+  /** Ascending order, nulls last */
+  AscNullsLast = 'AscNullsLast',
+  /** Descending order, nulls first */
+  DescNullsFirst = 'DescNullsFirst',
+  /** Descending order, nulls last */
+  DescNullsLast = 'DescNullsLast'
+}
+
+export type PageInfo = {
+  __typename?: 'PageInfo';
+  endCursor?: Maybe<Scalars['String']['output']>;
+  hasNextPage: Scalars['Boolean']['output'];
+  hasPreviousPage: Scalars['Boolean']['output'];
+  startCursor?: Maybe<Scalars['String']['output']>;
+};
+
+/** The root type for querying data */
+export type Query = {
+  __typename?: 'Query';
+  /** A pagable collection of type `bills` */
+  billsCollection?: Maybe<BillsConnection>;
+  /** Retrieve a record by its `ID` */
+  node?: Maybe<Node>;
+  /** A pagable collection of type `profiles` */
+  profilesCollection?: Maybe<ProfilesConnection>;
+};
+
+
+/** The root type for querying data */
+export type QueryBillsCollectionArgs = {
+  after?: InputMaybe<Scalars['Cursor']['input']>;
+  before?: InputMaybe<Scalars['Cursor']['input']>;
+  filter?: InputMaybe<BillsFilter>;
+  first?: InputMaybe<Scalars['Int']['input']>;
+  last?: InputMaybe<Scalars['Int']['input']>;
+  orderBy?: InputMaybe<Array<BillsOrderBy>>;
+};
+
+
+/** The root type for querying data */
+export type QueryNodeArgs = {
+  nodeId: Scalars['ID']['input'];
+};
+
+
+/** The root type for querying data */
+export type QueryProfilesCollectionArgs = {
+  after?: InputMaybe<Scalars['Cursor']['input']>;
+  before?: InputMaybe<Scalars['Cursor']['input']>;
+  filter?: InputMaybe<ProfilesFilter>;
+  first?: InputMaybe<Scalars['Int']['input']>;
+  last?: InputMaybe<Scalars['Int']['input']>;
+  orderBy?: InputMaybe<Array<ProfilesOrderBy>>;
+};
+
+/** Boolean expression comparing fields on type "String" */
+export type StringFilter = {
+  eq?: InputMaybe<Scalars['String']['input']>;
+  gt?: InputMaybe<Scalars['String']['input']>;
+  gte?: InputMaybe<Scalars['String']['input']>;
+  ilike?: InputMaybe<Scalars['String']['input']>;
+  in?: InputMaybe<Array<Scalars['String']['input']>>;
+  iregex?: InputMaybe<Scalars['String']['input']>;
+  is?: InputMaybe<FilterIs>;
+  like?: InputMaybe<Scalars['String']['input']>;
+  lt?: InputMaybe<Scalars['String']['input']>;
+  lte?: InputMaybe<Scalars['String']['input']>;
+  neq?: InputMaybe<Scalars['String']['input']>;
+  regex?: InputMaybe<Scalars['String']['input']>;
+  startsWith?: InputMaybe<Scalars['String']['input']>;
+};
+
+/** Boolean expression comparing fields on type "Time" */
+export type TimeFilter = {
+  eq?: InputMaybe<Scalars['Time']['input']>;
+  gt?: InputMaybe<Scalars['Time']['input']>;
+  gte?: InputMaybe<Scalars['Time']['input']>;
+  in?: InputMaybe<Array<Scalars['Time']['input']>>;
+  is?: InputMaybe<FilterIs>;
+  lt?: InputMaybe<Scalars['Time']['input']>;
+  lte?: InputMaybe<Scalars['Time']['input']>;
+  neq?: InputMaybe<Scalars['Time']['input']>;
+};
+
+/** Boolean expression comparing fields on type "UUID" */
+export type UuidFilter = {
+  eq?: InputMaybe<Scalars['UUID']['input']>;
+  in?: InputMaybe<Array<Scalars['UUID']['input']>>;
+  is?: InputMaybe<FilterIs>;
+  neq?: InputMaybe<Scalars['UUID']['input']>;
+};
+
+export type Bills = Node & {
+  __typename?: 'bills';
+  id: Scalars['BigInt']['output'];
+  inserted_at: Scalars['Datetime']['output'];
+  is_complete?: Maybe<Scalars['Boolean']['output']>;
+  /** Globally Unique Record Identifier */
+  nodeId: Scalars['ID']['output'];
+  task?: Maybe<Scalars['String']['output']>;
+  user_id: Scalars['UUID']['output'];
+};
+
+export type BillsConnection = {
+  __typename?: 'billsConnection';
+  edges: Array<BillsEdge>;
+  pageInfo: PageInfo;
+};
+
+export type BillsDeleteResponse = {
+  __typename?: 'billsDeleteResponse';
+  /** Count of the records impacted by the mutation */
+  affectedCount: Scalars['Int']['output'];
+  /** Array of records impacted by the mutation */
+  records: Array<Bills>;
+};
+
+export type BillsEdge = {
+  __typename?: 'billsEdge';
+  cursor: Scalars['String']['output'];
+  node: Bills;
+};
+
+export type BillsFilter = {
+  id?: InputMaybe<BigIntFilter>;
+  inserted_at?: InputMaybe<DatetimeFilter>;
+  is_complete?: InputMaybe<BooleanFilter>;
+  nodeId?: InputMaybe<IdFilter>;
+  task?: InputMaybe<StringFilter>;
+  user_id?: InputMaybe<UuidFilter>;
+};
+
+export type BillsInsertInput = {
+  inserted_at?: InputMaybe<Scalars['Datetime']['input']>;
+  is_complete?: InputMaybe<Scalars['Boolean']['input']>;
+  task?: InputMaybe<Scalars['String']['input']>;
+  user_id?: InputMaybe<Scalars['UUID']['input']>;
+};
+
+export type BillsInsertResponse = {
+  __typename?: 'billsInsertResponse';
+  /** Count of the records impacted by the mutation */
+  affectedCount: Scalars['Int']['output'];
+  /** Array of records impacted by the mutation */
+  records: Array<Bills>;
+};
+
+export type BillsOrderBy = {
+  id?: InputMaybe<OrderByDirection>;
+  inserted_at?: InputMaybe<OrderByDirection>;
+  is_complete?: InputMaybe<OrderByDirection>;
+  task?: InputMaybe<OrderByDirection>;
+  user_id?: InputMaybe<OrderByDirection>;
+};
+
+export type BillsUpdateInput = {
+  inserted_at?: InputMaybe<Scalars['Datetime']['input']>;
+  is_complete?: InputMaybe<Scalars['Boolean']['input']>;
+  task?: InputMaybe<Scalars['String']['input']>;
+  user_id?: InputMaybe<Scalars['UUID']['input']>;
+};
+
+export type BillsUpdateResponse = {
+  __typename?: 'billsUpdateResponse';
+  /** Count of the records impacted by the mutation */
+  affectedCount: Scalars['Int']['output'];
+  /** Array of records impacted by the mutation */
+  records: Array<Bills>;
+};
+
+export type Profiles = Node & {
+  __typename?: 'profiles';
+  avatar_url?: Maybe<Scalars['String']['output']>;
+  full_name?: Maybe<Scalars['String']['output']>;
+  id: Scalars['UUID']['output'];
+  /** Globally Unique Record Identifier */
+  nodeId: Scalars['ID']['output'];
+  updated_at?: Maybe<Scalars['Datetime']['output']>;
+  username?: Maybe<Scalars['String']['output']>;
+  website?: Maybe<Scalars['String']['output']>;
+};
+
+export type ProfilesConnection = {
+  __typename?: 'profilesConnection';
+  edges: Array<ProfilesEdge>;
+  pageInfo: PageInfo;
+};
+
+export type ProfilesDeleteResponse = {
+  __typename?: 'profilesDeleteResponse';
+  /** Count of the records impacted by the mutation */
+  affectedCount: Scalars['Int']['output'];
+  /** Array of records impacted by the mutation */
+  records: Array<Profiles>;
+};
+
+export type ProfilesEdge = {
+  __typename?: 'profilesEdge';
+  cursor: Scalars['String']['output'];
+  node: Profiles;
+};
+
+export type ProfilesFilter = {
+  avatar_url?: InputMaybe<StringFilter>;
+  full_name?: InputMaybe<StringFilter>;
+  id?: InputMaybe<UuidFilter>;
+  nodeId?: InputMaybe<IdFilter>;
+  updated_at?: InputMaybe<DatetimeFilter>;
+  username?: InputMaybe<StringFilter>;
+  website?: InputMaybe<StringFilter>;
+};
+
+export type ProfilesInsertInput = {
+  avatar_url?: InputMaybe<Scalars['String']['input']>;
+  full_name?: InputMaybe<Scalars['String']['input']>;
+  id?: InputMaybe<Scalars['UUID']['input']>;
+  updated_at?: InputMaybe<Scalars['Datetime']['input']>;
+  username?: InputMaybe<Scalars['String']['input']>;
+  website?: InputMaybe<Scalars['String']['input']>;
+};
+
+export type ProfilesInsertResponse = {
+  __typename?: 'profilesInsertResponse';
+  /** Count of the records impacted by the mutation */
+  affectedCount: Scalars['Int']['output'];
+  /** Array of records impacted by the mutation */
+  records: Array<Profiles>;
+};
+
+export type ProfilesOrderBy = {
+  avatar_url?: InputMaybe<OrderByDirection>;
+  full_name?: InputMaybe<OrderByDirection>;
+  id?: InputMaybe<OrderByDirection>;
+  updated_at?: InputMaybe<OrderByDirection>;
+  username?: InputMaybe<OrderByDirection>;
+  website?: InputMaybe<OrderByDirection>;
+};
+
+export type ProfilesUpdateInput = {
+  avatar_url?: InputMaybe<Scalars['String']['input']>;
+  full_name?: InputMaybe<Scalars['String']['input']>;
+  id?: InputMaybe<Scalars['UUID']['input']>;
+  updated_at?: InputMaybe<Scalars['Datetime']['input']>;
+  username?: InputMaybe<Scalars['String']['input']>;
+  website?: InputMaybe<Scalars['String']['input']>;
+};
+
+export type ProfilesUpdateResponse = {
+  __typename?: 'profilesUpdateResponse';
+  /** Count of the records impacted by the mutation */
+  affectedCount: Scalars['Int']['output'];
+  /** Array of records impacted by the mutation */
+  records: Array<Profiles>;
+};
