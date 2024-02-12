59c36cedd3aa1f8d88d7365394d02cdbd2a62504
Jason Ruesch <jruesch@degreed.com>
2024-02-12 15:07:34 -0500

    fix(bills): use test color

--- apps/bills/src/app/app.tsx
+++ apps/bills/src/app/app.tsx
@@ -1,7 +1,7 @@
 export function App() {
   return (
     <div className="min-h-dvh grid place-content-center text-center">
-      <h1 className="text-4xl font-bold">Bills</h1>
+      <h1 className="text-4xl font-bold text-test">Bills</h1>
     </div>
   );
 }
