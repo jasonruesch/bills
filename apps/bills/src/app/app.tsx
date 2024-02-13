4e40022f91121a8e4b967bfdb54999851d00db80
Jason Ruesch <jruesch@degreed.com>
2024-02-12 21:49:00 -0500

    feat(bills): display test color box

--- apps/bills/src/app/app.tsx
+++ apps/bills/src/app/app.tsx
@@ -1,7 +1,11 @@
 export function App() {
   return (
     <div className="min-h-dvh grid place-content-center text-center">
-      <h1 className="text-4xl font-bold text-test">Bills</h1>
+      <h1 className="text-4xl font-bold">Bills</h1>
+
+      <div className="ring-8 ring-test p-8 mt-8">
+        <p className="text-xl font-semibold text-test">Test</p>
+      </div>
     </div>
   );
 }
