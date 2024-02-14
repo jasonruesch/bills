f1564c007b3ac4caf5a203546c29177e108de60c
Jason Ruesch <jruesch@degreed.com>
2024-02-13 22:49:58 -0500

    feat(bills): update bg opacity to 5

--- apps/bills/src/app/app.tsx
+++ apps/bills/src/app/app.tsx
@@ -3,7 +3,7 @@ export function App() {
     <div className="min-h-dvh grid place-content-center text-center">
       <h1 className="text-4xl font-bold">Bills</h1>
 
-      <div className="ring-8 ring-test p-8 mt-8 bg-test/20">
+      <div className="ring-8 ring-test p-8 mt-8 bg-test/5">
         <p className="text-xl font-semibold text-test">Test</p>
       </div>
     </div>
