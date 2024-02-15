4d5f5206ec5d34051b0b4abe5e4ef5da0057d048
Jason Ruesch <jruesch@degreed.com>
2024-02-15 07:19:10 -0500

    feat(bills): increment value

--- apps/bills/src/app/app.tsx
+++ apps/bills/src/app/app.tsx
@@ -20,7 +20,7 @@ export function App() {
           {allBills.map((bill) => (
             <div
               key={bill.id}
-              className="p-4 border border-test/10 rounded-md col-span-4"
+              className="p-4 border border-test/20 rounded-md col-span-4"
             >
               <p className="font-semibold">{bill.name}</p>
               <p>{formatDate(bill.createdAt)}</p>
